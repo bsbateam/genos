bac9e031b5430304881dd97c748e56d6fb33cdf9
Geno Ferollo <sakhib.orzklv@icloud.com>
2021-04-02 15:58:55 +0500

    changing urls

--- components/Programs/Genemator.tsx
+++ components/Programs/Genemator.tsx
@@ -2,7 +2,7 @@ import styles from '@/styles/Programs/Blog.module.scss';
 
 import type { AppComponent } from '@/types/utils/programs';
 
-const GENEMATOR_HOME_PAGE = 'https://genemator.me/';
+const GENEMATOR_HOME_PAGE = 'https://genemator.uz/';
 
 const Genemator: React.FC<AppComponent> = () => (
   <iframe className={styles.blog} title="Genemator" src={GENEMATOR_HOME_PAGE} />
